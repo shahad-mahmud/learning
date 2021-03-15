20d42f4c621140dd83266a44cb063f0b112421ef
Shahad Mahmud <shahad9381@gmail.com>
2021-03-15 15:49:02 +0600

    text justification

---
+++ text_justification/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace text_justification
+{
+    class Program{
+        static List<string> SplitText(string text){
+            var words = new List<string>();
+
+            int len = text.Length;
+            string temp_word = "";
+
+            foreach (var cha in text){
+                if (cha != ' '){
+                    temp_word += cha;
+                }else{
+                    words.Add(temp_word);
+                    temp_word = "";
+                }
+            }
+
+            if (temp_word != ""){
+                words.Add(temp_word);
+            }
+
+            return words;
+        }
+
+        static void Justify(string text, int line_width){
+            var words = SplitText(text);
+
+            int start_index = 0, n_words = 0, line_len = 0;
+        }
+        static void Main(string[] args)
+        {
+            string text = "This is a text. I will try to justify this text using c sharp.";
+            int line_width = 15;
+
+            var words = SplitText(text);
+
+            foreach (var word  in words){
+                Console.WriteLine(word);
+            }
+            Console.WriteLine(words);
+        }
+    }
+}
