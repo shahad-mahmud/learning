1383c3bf8a2b6f3909adbfedcc4a7a1a7bd2cd62
Shahad Mahmud <shahad9381@gmail.com>
2021-03-15 15:19:56 +0600

    first commit

---
+++ learning_c_sharp/learning_c_s/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace learning_c_s
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi();
+        }
+
+        static void SayHi(){
+            Console.WriteLine("This is the first method");
+        }
+    }
+}
