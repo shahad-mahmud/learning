97c06dfe43fec9d2e04722c6f6ec8b941d5d46f6
Shahad Mahmud <shahad9381@gmail.com>
2021-03-16 01:25:43 +0600

    text justification done

---
+++ text_justification/Old_process.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace text_justification
+{
+    class Program{
+        static List<string> SplitText(string text){
+            var words = new List<string>();
+
+            int len = text.Length;
+            string temp_word = "";
+
+            foreach (var cha in text){
+                if (cha != ' '){
+                    temp_word += cha;
+                }else{
+                    words.Add(temp_word);
+                    temp_word = "";
+                }
+            }
+
+            if (temp_word != ""){
+                words.Add(temp_word);
+            }
+
+            return words;
+        }
+
+        static string JoinLine(List<string> words, int start_pos, int end_pos, int  n_space){
+            int n_words = end_pos - start_pos + 1;
+            string justified_line = "";
+
+            foreach (var word in words){
+                Console.WriteLine("Loop 2");
+                justified_line += word;
+                n_words--;
+
+                double n_space_to_add = (double)n_space / n_words;
+                int space_to_add = (int)Math.Ceiling(n_space_to_add);
+                for(int i = 0; i < space_to_add; i++){
+                    justified_line += " ";
+                }
+
+                n_space -= space_to_add;
+            }
+
+            return justified_line;
+        }
+
+        static void Justify(string text, int line_width){
+            Console.WriteLine("call funtion");
+            var words = SplitText(text);
+            int len = words.Count;
+
+            int start_index = 0, n_words = 0, line_len = 0, processed_words = 0;
+            var justified_line = new List<string>();
+
+            foreach (var word in words){
+                Console.WriteLine("main loop");
+                n_words++;
+
+                int possible_line_len = line_len + word.Length + n_words - 1;
+
+                if(possible_line_len == line_width){
+                    string j_line = JoinLine(
+                        words,
+                        start_index,
+                        processed_words,
+                        processed_words - start_index
+                    );
+
+                    justified_line.Add(j_line);
+
+                    start_index = processed_words + 1;
+                    n_words = 0; 
+                    line_len = 0;
+                }
+                else if(possible_line_len > line_width){
+                    string j_line = JoinLine(
+                        words,
+                        start_index,
+                        processed_words - 1,
+                        processed_words - line_len
+                    );
+
+                    justified_line.Add(j_line);
+
+                    start_index = processed_words;
+                    n_words = 1;
+                    line_len = word.Length;
+                }
+                else{
+                    line_len += word.Length;
+                }
+
+                processed_words++;
+            }
+
+            if(line_len > 0){
+                string j_line = JoinLine(
+                        words,
+                        start_index,
+                        len - 1,
+                        line_len - 1
+                    );
+
+                justified_line.Add(j_line);
+            }
+
+            foreach(var line in justified_line){
+                Console.WriteLine(line);
+            }
+        }
+        static void N_Main(string[] args)
+        {
+            string text = "This is a text. I will try to justify this text using c sharp.";
+            int line_width = 15;
+
+            Justify(text, 15);
+        }
+    }
+}
--- text_justification/Program.cs
+++ text_justification/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace text_justification
 {
-    class Program{
+    class New_Program{
         static List<string> SplitText(string text){
             var words = new List<string>();
 
@@ -25,24 +25,72 @@ namespace text_justification
             }
 
             return words;
+        } 
+
+        static void PrintLine(List<string> words, int start_index, int end_index, 
+                    int space_needed){
+            int word_count = end_index - start_index + 1;
+            string line = "";
+
+            for(int i = start_index; i <= end_index; i++){
+                line += words[i];
+                word_count--;
+                int n_space = (int) Math.Ceiling((double) space_needed / word_count);
+                // Console.WriteLine($"space needed {space_needed}, words: {word_count}");
+                // Console.WriteLine(n_space);
+
+                for(int j = 0; j < n_space; j++){
+                    line += " ";
+                }
+                space_needed -= n_space;
+            }
+
+            Console.WriteLine(line);
         }
 
         static void Justify(string text, int line_width){
             var words = SplitText(text);
+            int len = words.Count;
 
-            int start_index = 0, n_words = 0, line_len = 0;
+            int temp_len = 0, start_index = 0, current_line_length = 0;
+            int n_words_in_current_line = 0;
+
+            for(int i = 0; i < len; i++){
+                n_words_in_current_line++;
+                temp_len = current_line_length + words[i].Length + n_words_in_current_line - 1;
+                // Console.WriteLine($"n_words {n_words_in_current_line}, crnt line len {current_line_length} temp len {temp_len}");
+            
+                if (temp_len == line_width){
+                    // Console.WriteLine("if 1");
+                    PrintLine(words, start_index, i, i - start_index);                    
+                    start_index = i + 1;
+                    temp_len = 0;
+                    n_words_in_current_line = 0;
+                    current_line_length = 0;
+                }
+                else if (temp_len > line_width){
+                    // Console.WriteLine("if 2");
+                    PrintLine(words, start_index, i -1 , line_width - current_line_length);                    
+                    start_index = i;
+                    temp_len = 0;
+                    n_words_in_current_line = 1;
+                    current_line_length = words[i].Length;
+                }
+                else{
+                    current_line_length += words[i].Length;
+                }
+            }
+
+            if(n_words_in_current_line > 0){
+                PrintLine(words, start_index, len -1 , n_words_in_current_line - 1);
+            }
         }
         static void Main(string[] args)
         {
-            string text = "This is a text. I will try to justify this text using c sharp.";
-            int line_width = 15;
+            string text = "This is a text. I will try to justify this text using c sharp. I think this will be fun.";
+            int line_width = 25;
 
-            var words = SplitText(text);
-
-            foreach (var word  in words){
-                Console.WriteLine(word);
-            }
-            Console.WriteLine(words);
+            Justify(text, line_width);
         }
     }
 }
