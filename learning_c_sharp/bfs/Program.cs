beae906e7a8ffdae25d140dbcdddc18c80000457
Shahad Mahmud <shahad9381@gmail.com>
2021-03-18 08:43:15 +0600

    bfs done

---
+++ learning_c_sharp/bfs/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bfs
+{
+    class Program
+    {
+        static void AddToGraph(ref Dictionary<string, HashSet<string>> graph, string node1, string node2){
+            if(graph.ContainsKey(node1)){
+                graph[node1].Add(node2);
+            }else{
+                HashSet<string> temp_nodes = new HashSet<string>(){node2};
+                graph[node1] = temp_nodes;
+            }
+
+            if(graph.ContainsKey(node2)){
+                graph[node2].Add(node1);
+            }else{
+                HashSet<string> temp_nodes = new HashSet<string>(){node1};
+                graph[node2] = temp_nodes;
+            }
+        }
+
+        static void bfs(Dictionary<string, HashSet<string>> graph, string root){
+            List<string> visited = new List<string>();
+            Queue<string> queue = new Queue<string>();
+
+            queue.Enqueue(root);
+            visited.Add(root);
+
+            while(queue.Count > 0){
+                string current_root = queue.Dequeue();
+                Console.Write(current_root + " ");
+
+                if(graph.ContainsKey(current_root)){
+                    foreach(var neighbour in graph[current_root]){
+                        if(!visited.Contains(neighbour)){
+                            visited.Add(neighbour);
+                            queue.Enqueue(neighbour);
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var graph = new Dictionary<string, HashSet<string>>();
+
+            AddToGraph(ref graph, "a", "b");
+            AddToGraph(ref graph, "a", "c");
+            AddToGraph(ref graph, "b", "d");
+            AddToGraph(ref graph, "b", "e");
+            AddToGraph(ref graph, "d", "h");
+            AddToGraph(ref graph, "d", "i");
+            AddToGraph(ref graph, "e", "1");
+            AddToGraph(ref graph, "e", "2");
+            AddToGraph(ref graph, "c", "f");
+            AddToGraph(ref graph, "c", "g");
+
+            bfs(graph, "a");
+
+        }
+    }
+}
